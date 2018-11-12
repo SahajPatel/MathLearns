fb8f66cafdb4e9485b58db86e20ca834287d3fb0
Amhepel <amhepel@buffalo.edu>
2018-11-12 15:45:24 -0500

    Subtraction.

--- app/src/main/java/com/example/sahaj/mathlearns/Subtraction.java
+++ app/src/main/java/com/example/sahaj/mathlearns/Subtraction.java
@@ -1,36 +1,36 @@
 package com.example.sahaj.mathlearns;
 
 import java.util.Random;
-public class Addition {
+public class Subtraction {
 
 
 
-//    int submitted = Integer.parseInt(answerText.getText().toString());
+    //    int submitted = Integer.parseInt(answerText.getText().toString());
     static Random randomGen  = new Random();
     static int num1 = 0;
     static int num2 = 0;
     static int result  = 0;
     public static int isRight(Integer submitted){
-        result  = num1 + num2;
+        result  = num1 - num2;
         if(submitted == result) {
             return 1;
         }
         if(submitted == null){
             return 0;
-            }
+        }
         if(submitted != result && !(submitted == null)){
             return -1;
         }
         return 0;
     }
-    public static String askMe(){
+    public static String askMes(){
         num1 = randomGen.nextInt(20-1)+1;
         num2 = randomGen.nextInt(20-1)+1;
-        return "What is " + num1 + " + " + num2;
+        return "What is " + num1 + " - " + -num2;
     }
-    public static String askMe2(){
+    public static String askMes2(){
         num1 = randomGen.nextInt(100-1)+1;
         num2 = randomGen.nextInt(100-1)+1;
-        return "What is " + num1 + " + " + num2;
+        return "What is " + num1 + " - " + -num2;
     }
 }
---
+++ app/src/main/java/com/example/sahaj/mathlearns/subLevTwo.java
@@ -0,0 +1,55 @@
+package com.example.sahaj.mathlearns;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.sahaj.mathlearns.Addition;
+import com.example.sahaj.mathlearns.R;
+
+import java.util.Random;
+
+public class subLevTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lev_one);
+        EditText answerText;
+        final TextView Questions = (TextView) findViewById(R.id.Questions);
+        Questions.setText(Subtraction.askMes2());
+        Button Submit = (Button) findViewById(R.id.Submit);
+        Submit.setOnClickListener(new View.OnClickListener() {
+                                      @Override
+                                      public void onClick(View v) {
+                                          EditText answerText = (EditText) findViewById(R.id.answerText);
+                                          Integer submitted = 0;
+                                          TextView correctTextView = (TextView) findViewById(R.id.Correct);
+                                          if(answerText.getText().length()==0){
+                                              correctTextView.setText("Please submit an answer");
+                                          }else{
+                                              if(answerText.getText().length()!=0) {
+                                                  submitted = Integer.parseInt(answerText.getText().toString());
+                                              }}
+                                          //    int submitted = Integer.parseInt(answerText.getText().toString());
+                                          Random randomGen  = new Random();
+                                          int num1 = randomGen.nextInt();
+                                          int num2 = randomGen.nextInt();
+                                          int result  = num1 - num2;
+
+                                          if(Addition.isRight(submitted) == 1){
+                                              correctTextView.setText("Correct");
+                                              Questions.setText(Subtraction.askMes2());
+                                          }else {
+                                              if (Subtraction.isRight(submitted) == -1) {
+                                                  correctTextView.setText("Try Again");
+                                              }
+                                          }
+                                      }
+                                  }
+        );
+    }
+}
